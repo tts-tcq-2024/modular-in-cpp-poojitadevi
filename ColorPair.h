94073bd101c466f20cb2f2ce9acf526bc92bf52d
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-26 19:27:23 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -1,6 +1,5 @@
-// ColorPair.h
-#ifndef COLOR_PAIR_H
-#define COLOR_PAIR_H
+#ifndef COLORPAIR_H
+#define COLORPAIR_H
 
 #include <string>
 
@@ -9,27 +8,21 @@ namespace TelCoColorCoder {
     enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
     enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-    extern const char* MajorColorNames[];
-    extern const char* MinorColorNames[];
-
-    extern const int numberOfMajorColors;
-    extern const int numberOfMinorColors;
-
     class ColorPair {
-    private:
-        MajorColor majorColor;
-        MinorColor minorColor;
-    public:
-        ColorPair(MajorColor major, MinorColor minor) : majorColor(major), minorColor(minor) {}
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor) : majorColor(major), minorColor(minor) {}
 
-        MajorColor getMajor() const;
-        MinorColor getMinor() const;
-        std::string ToString() const;
+            // Inline function definitions moved to the header file
+            inline MajorColor getMajor() const { return majorColor; }
+            inline MinorColor getMinor() const { return minorColor; }
+
+            std::string ToString() const;
     };
 
-    // Function to print the reference manual for wiring personnel
     void PrintColorCodingManual();
-
 }
 
-#endif // COLOR_PAIR_H
+#endif
