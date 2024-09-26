7db69e2cc16961668950403fd12eb972ff591bf6
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-26 17:07:33 +0530

    Create ColorPair.h

---
+++ ColorPair.h
@@ -0,0 +1,46 @@
+// ColorPair.h
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+#include <string>
+
+namespace TelCoColorCoder {
+
+    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+
+    const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    class ColorPair {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor) : majorColor(major), minorColor(minor) {}
+
+        MajorColor getMajor() {
+            return majorColor;
+        }
+
+        MinorColor getMinor() {
+            return minorColor;
+        }
+
+        std::string ToString() {
+            std::string colorPairStr = MajorColorNames[majorColor];
+            colorPairStr += " ";
+            colorPairStr += MinorColorNames[minorColor];
+            return colorPairStr;
+        }
+    };
+}
+
+#endif // COLOR_PAIR_H
