950b67951f62c63d761bdc840beaf0e9842c2ae6
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-26 19:38:58 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -6,6 +6,8 @@ namespace TelCoColorCoder {
 
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+    // Define the extern variables here
     const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
     const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
