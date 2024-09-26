3ffd4f16bdccb982458740880205184692bcfd84
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-26 19:33:33 +0530

    Update GetColorFromPairNumber.cpp

--- GetColorFromPairNumber.cpp
+++ GetColorFromPairNumber.cpp
@@ -1,7 +1,4 @@
-// GetColorFromPairNumber.cpp
 #include "ColorPair.h"
-#include <iostream>
-#include <cassert>
 
 namespace TelCoColorCoder {
 
@@ -11,12 +8,5 @@ namespace TelCoColorCoder {
         MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return ColorPair(majorColor, minorColor);
     }
-}
 
-// Test function for getting colors from pair number
-void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor) {
-    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor); 
 }
