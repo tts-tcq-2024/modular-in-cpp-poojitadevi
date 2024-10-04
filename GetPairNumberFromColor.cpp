d2b075c47467d7a2aec9ada32d23e1a86a933865
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-10-04 09:43:48 +0530

    Delete GetPairNumberFromColor.cpp

--- GetPairNumberFromColor.cpp
+++
@@ -1,9 +0,0 @@
-#include "ColorPair.h"
-
-namespace TelCoColorCoder {
-
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-    }
-
-}
